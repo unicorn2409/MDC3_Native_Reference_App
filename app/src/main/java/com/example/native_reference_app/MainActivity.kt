9b1d366538cb7a3570cbeef63034d00fa50bfb07
GithubAbhishek09 <GitHub@24899>
2024-08-14 17:51:27 +0530

    This is my second commit - Toast Added

--- app/src/main/java/com/example/native_reference_app/MainActivity.kt
+++ app/src/main/java/com/example/native_reference_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.native_reference_app
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        showToast()
+    }
+
+    private fun showToast() {
+        Toast.makeText(this, "This is my first App", Toast.LENGTH_SHORT).show()
     }
 }
